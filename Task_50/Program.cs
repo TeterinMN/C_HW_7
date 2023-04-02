2dcb565d39b04f7502e6416e1e0d4ba15d7a0b58
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-02 21:41:20 +0900

    Закончил задание №50

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.Clear();
+int[,] array = new int[,]
+{
+            {1, 4, 7, 2},
+            {5, 9, 2, 3},
+            {8, 4, 2, 4}
+};
+
+Console.Write("Введите номер столбца: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите номер строки: ");
+int lines = int.Parse(Console.ReadLine()!);
+
+if (lines < 0 || lines >= array.GetLength(0) || columns < 0 || columns >= array.GetLength(1))
+{
+    Console.WriteLine("Такой позиции нет");
+}
+else
+{
+    Console.WriteLine("Элемент с заданной позицией: " + array[lines, columns]);
+}
