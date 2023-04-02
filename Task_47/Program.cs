7795d2c7a7409ec10fff8f263cf801d61672ad0c
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-02 21:29:36 +0900

    Закончил задание №47

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int m = 3;
+int n = 4;
+double[,] array = new double[m, n];
+
+Random rand = new Random();
+// Заполнение массива случайными числами
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = (rand.NextDouble() * 20) - 10;
+    }
+}
+
+// Вывод массива на экран
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write("{0:0.0} ", array[i, j]);
+    }
+    Console.WriteLine();
+}
